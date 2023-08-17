45e00b9261e03b1edfc5a9988146878c4441810e
Cassiano-Luz <cassianovl@hotmail.com>
2023-08-17 17:43:18 -0300

    outros atualizado

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,14 @@ btn.addEventListener('click', function() {
         servicos.style.display = 'block';
     }
 });
+
+var btn = document.querySelector('#show-or-hide-qs');
+var outros = document.querySelector('.outros');
+
+btn.addEventListener('click', function() {
+    if(outros.style.display === 'block') {
+        outros.style.display = 'none';
+    } else {
+        outros.style.display = 'block';
+    }
+});
